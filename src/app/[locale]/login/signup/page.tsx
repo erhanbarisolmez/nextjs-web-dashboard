a3b202468044243efd1648da03b01567ec7acc1b
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2023-11-06 14:31:36 +0300

    6.11.2023 14:31

--- src/app/[locale]/login/signup/page.tsx
+++ src/app/[locale]/login/signup/page.tsx
@@ -1,14 +1,10 @@
 import SignUpContent from "../components/SignUpContent";
-import LoginLayout from "../layout";
 // import LoginLayout from "../layout";
 
 export default function SignUp() {
   return (
     <>
-    
-    <LoginLayout>
       <SignUpContent/>
-    </LoginLayout>
     </>
     )
 }
